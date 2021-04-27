dd57d52a5c1dc44f079094314006657677ed7882
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2021-04-27 11:09:29 +0300

    impruve const to them

--- src/body-theme-change.js
+++ src/body-theme-change.js
@@ -1,20 +1,25 @@
 const bodyContainer = document.querySelector('body')
 const themeCheckbox = document.querySelector('.theme-switch__toggle')
 
-const THEM_KEY ='them-checkbox'
+const THEM_KEY = 'them-checkbox'
+const THEM_VALUE = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
 const checkboxSaved = localStorage.getItem(THEM_KEY)
 if (checkboxSaved==='checked') {
     themeCheckbox.checked = 'true'
-    bodyContainer.classList.add('dark-theme')
+    bodyContainer.classList.add(THEM_VALUE.DARK)
 }
 
 themeCheckbox.addEventListener('change', onCheckboxChange)
 function onCheckboxChange () {
     if (themeCheckbox.checked) {
-        bodyContainer.classList.add('dark-theme')
+        bodyContainer.classList.add(THEM_VALUE.DARK)
         localStorage.setItem(THEM_KEY, 'checked')
         return;
     }
-    bodyContainer.classList.replace('dark-theme', 'light-theme')
+    bodyContainer.classList.replace(THEM_VALUE.DARK, THEM_VALUE.LIGHT)
     localStorage.setItem(THEM_KEY, 'unchecked')
 }
