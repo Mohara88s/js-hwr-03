2aceff1e0207fe662531754b6156c478a441013e
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2021-04-27 09:05:44 +0300

    done

--- src/js-menu.js
+++ src/js-menu.js
@@ -1,5 +1,6 @@
 import menuTemplate from './menu-items.hbs'
 import menuItems from './menu.json'
 
+const menuMarkup = menuTemplate(menuItems)
 const menuList = document.querySelector('.js-menu')
-
+menuList.insertAdjacentHTML('beforeend', menuMarkup)
