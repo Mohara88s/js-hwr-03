81a6fda827791c59cb523206bf8314e1f6a79737
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2021-04-27 10:02:28 +0300

    fix them-checkbox in localstorage

--- src/body-theme-change.js
+++ src/body-theme-change.js
@@ -1,7 +1,7 @@
 const bodyContainer = document.querySelector('body')
 const themeCheckbox = document.querySelector('.theme-switch__toggle')
 
-const checkboxSaved = localStorage.getItem('checkbox')
+const checkboxSaved = localStorage.getItem('them-checkbox')
 if (checkboxSaved==='checked') {
     themeCheckbox.checked = 'true'
     bodyContainer.classList.add('dark-theme')
@@ -11,9 +11,9 @@ themeCheckbox.addEventListener('change', onCheckboxChange)
 function onCheckboxChange () {
     if (themeCheckbox.checked) {
         bodyContainer.classList.add('dark-theme')
-        localStorage.setItem('checkbox', 'checked')
+        localStorage.setItem('them-checkbox', 'checked')
         return;
     }
     bodyContainer.classList.replace('dark-theme', 'light-theme')
-    localStorage.setItem('checkbox', 'unchecked')
+    localStorage.setItem('them-checkbox', 'unchecked')
 }
